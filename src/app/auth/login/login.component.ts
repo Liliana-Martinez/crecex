ecb6853ae427b60912fb57ec2d93f82c5e2a4afc
Liliana-Martinez <lilia001125@gmail.com>
2025-03-24 18:40:48 -0600

    Se agrego funcion para llamar al servicio

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -13,30 +13,36 @@ import { AuthService } from '../../core/services/auth.service';
 export class LoginComponent implements OnInit {
 
   //Se crea el formGroup de tipo Auth
-  auth!: FormGroup;
+  loginForm!: FormGroup;
+  errorMessage: string = '';
 
   constructor(private authService: AuthService, private router: Router) {}
   
   //Se capturan los valores de los inputs
   ngOnInit() {
-    this.auth = new FormGroup({
+    this.loginForm = new FormGroup({
       user: new FormControl('', [Validators.required]),
       password: new FormControl('', [Validators.required])
     });
   }
 
   login() {
-    if(this.auth.valid) {
-      const credentials = this.auth.value;
-
-      this.authService.login(credentials).subscribe(response => {
-        if(response.success) {
-          //Redirigir a home si el login es correcto
-          this.router.navigate(['/app/home']);
-        } else {
-          alert("Credenciales incorrectas")
-        }
-      })
+    if (this.loginForm.invalid) {
+      this.errorMessage = 'Por favor, complete todos los campos.';
+      return;
     }
+
+    //Llamar al servicio de autenticacion
+    this.authService.login(this.loginForm.value).subscribe({
+      next: (response) => {
+        console.log('Login exitoso: ', response);
+        this.authService.saveToken(response.token);
+        this.router.navigate(['/home']);
+      },
+      error: (err) => {
+        console.error('Error en el login', err);
+        this.errorMessage = 'Credenciales incorrectas. Intente de nuevo';
+      }
+    });
   }
 }
