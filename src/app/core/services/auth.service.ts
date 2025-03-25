2c52723e88831f1113e638854282fbb93c6fc7a9
Liliana-Martinez <lilia001125@gmail.com>
2025-03-24 18:43:51 -0600

    Se creo el servicio para autenticacion

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -2,17 +2,37 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Auth } from '../../models/auth';
 import { Observable } from 'rxjs';
+import { API_ROUTES } from '../constants/api-routes';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  private apiUrl = 'http://localhost:8080/api/auth/lo';
-
   constructor(private http: HttpClient) {}
 
-  login(auth: Auth): Observable<any> {
-    return this.http.post("http://localhost:8080/login", auth);
+  //Iniciar sesion
+  login(credentials: Auth): Observable<Auth> {
+    return this.http.post<Auth>(API_ROUTES.AUTH.LOGIN, credentials);
+  }
+
+  //Guardar token en el almacenamiento local
+  saveToken(token: string): void {
+    localStorage.setItem('token', token);
+  }
+
+  //Obtener el token de almacenamiento
+  getToken(): string | null {
+    return localStorage.getItem('token');
+  }
+
+  //Eliminar token (Cerrar sesion)
+  logout(): void {
+    localStorage.removeItem('token');
+  }
+
+  //Verificar si el usuario esta autenticado
+  isAunthenticated(): boolean {
+    return !!this.getToken();
   }
 }
