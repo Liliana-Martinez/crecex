a593fddcd33b6c8be0e6e98ef4302a2d5258d69b
Liliana-Martinez <lilia001125@gmail.com>
2025-05-07 12:49:36 -0600

    Otros cambios

--- src/app/features/credits/pagess/new/new.component.ts
+++ src/app/features/credits/pagess/new/new.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { SubmenuuComponent } from '../../componentss/submenuu/submenuu.component';
 import { FormCreditComponent } from '../../componentss/form-credit/form-credit.component';
 import { SearchBarComponent } from '../../../../shared/componentes/search-bar-client/search-bar.component';
-import { SaveButtonComponent } from '../../../../shared/componentes/save-button/save-button.component';
 import { PrintButtonComponent } from '../../../../shared/componentes/print-button/print-button.component';
 import { MatTableModule } from '@angular/material/table';
 import { CommonModule } from '@angular/common';
