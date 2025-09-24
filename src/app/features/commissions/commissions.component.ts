5d13f81ae13e5487dfe2a4568c02e2791cb37a03
ClaudiaRafaelSantos <clauyanethrs@gmail.com>
2025-09-24 13:34:31 -0600

    ...

--- src/app/features/commissions/commissions.component.ts
+++ src/app/features/commissions/commissions.component.ts
@@ -3,24 +3,29 @@ import { SearchBarZoneComponent } from '../../shared/componentes/search-bar-zone
 import { MatTableModule } from '@angular/material/table';
 import { Zone } from '../../models/Zone';
 import { CommissionsService } from '../../core/services/commissions.service';
+import { FormsModule } from '@angular/forms';
+import { SaveButtonComponent } from "../../shared/componentes/save-button/save-button.component";
 
 export interface commissionsData {
   collection: number;      // Gastos de cobranza
   collectionRate: number;  // Comisión
   numberCredits: number;   // No de créditos
+  extras: number;          // Extras
   totalAmount: number;     // Total (suma)
 }
 
 @Component({
   selector: 'app-commissions',
   standalone: true,
-  imports: [SearchBarZoneComponent, MatTableModule],
+  imports: [SearchBarZoneComponent, MatTableModule, FormsModule, SaveButtonComponent],
   templateUrl: './commissions.component.html',
   styleUrl: './commissions.component.css'
 })
 export class CommissionsComponent {
-  commissionsCol: string[] = ['collection', 'collectionRate', 'numberCredits', 'totalAmount'];
+  commissionsCol: string[] = ['collection', 'collectionRate', 'numberCredits', 'extras', 'totalAmount'];
   dataCommissions: commissionsData[] = [];
+  filtro: string = '';
+
 
   constructor(private commissionsService: CommissionsService) {}
 
@@ -36,6 +41,7 @@ export class CommissionsComponent {
         collection: resp.totalRecargos,                  // Gastos de cobranza
         collectionRate: resp.comision,                   // Comisión
         numberCredits: resp.total,                       // No de créditos
+        extras: resp.cantidad,
         totalAmount: resp.totalRecargos + resp.comision + resp.total // Suma
       };
 
